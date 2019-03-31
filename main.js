ae37eac8c0230e9a001f91871f4af0e4b4f87085
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-03-31 19:06:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-var money = 0
-function increase() {
-  money++
-  document.getElementById("money").textContent = "Points: " + money
+var money = 10
+var g1 = {
+  amount: 0,
+  cost: 0,
+  :function buy() {
+    if(money < g1.cost) return 0;
+    if(money > g1.cost) {
+      money-=g1.cost
+      g1.amount++
+      g1.cost = Math.round(g1.cost*1.1)
+    }
+  }
 }
+function format(num){
+  let power = Math.floor(Math.log10(num))
+  let mantissa = amount/Math.pow(10,power)
+  if(power < 8) return Math.round(amount)
+  if(power > 8) return mantissa.toFixed(2) + " * 10^" + power
+}
+
+document.getElementById("money").textContent = "You have " + money + " points."
+document.getElementById("gen1Amount").textContent = "You have " + g1.amount + " G1's."
+document.getElementById("gen1Cost").textContent = "It costs " + g1.cost + " points to buy a G1."
