55ed5d91e74d40cf51cf5b233777f02c15c8da84
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-03-31 19:58:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,13 +14,27 @@ var g1 = {
     }
   }
 }
+function updateThings() {
+  document.getElementById("money").textContent = "You have " + money + " points."
+  document.getElementById("gen1Amount").textContent = "You have " + g1.amount + " G1's."
+  document.getElementById("gen1Cost").textContent = "It costs " + g1.cost + " points to buy a G1."
+}
 function format(num){
   let power = Math.floor(Math.log10(num))
   let mantissa = amount/Math.pow(10,power)
   if(power < 8) return Math.round(amount)
   if(power > 8) return mantissa.toFixed(2) + " * 10^" + power
 }
-
+function tick(diff){
+  money+=(g1.amount*diff)
+}
+function gameLoop(){
+  var diff = (Date.now()-lastUpdate)/1000
+  tick(diff)
+  updateThings()
+  lastUpdate = Date.now()
+}
+setInterval(gameLoop,50)
 document.getElementById("money").textContent = "You have " + money + " points."
 document.getElementById("gen1Amount").textContent = "You have " + g1.amount + " G1's."
 document.getElementById("gen1Cost").textContent = "It costs " + g1.cost + " points to buy a G1."
