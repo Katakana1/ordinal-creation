3ee3d231f70a3fff0f5703bd1c3cac8b7b35b80f
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-03-31 21:23:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ var g1 = {
   amount: 0,
   cost: 0,
   buy: function() {
-    if(money < this.cost) return 0;
-    if(money >= this.cost) {
-      money-=this.cost
-      this.amount++
-      this.cost = Math.round(this.cost*1.1)
-      document.getElementById("money").textContent = "You have " + money + " points."
-      document.getElementById("gen1Amount").textContent = "You have " + g1.amount + " G1's."
-      document.getElementById("gen1Cost").textContent = "It costs " + g1.cost + " points to buy a G1."
+    if(money < g1.cost) return 0;
+    if(money >= g1.cost) {
+      money-=g1.cost
+      g1.amount++
+      g1.cost = Math.round(g1.cost*1.1)
+      document.getElementById("money").textContent = "You have " + format(money) + " points."
+      document.getElementById("gen1Amount").textContent = "You have " + format(g1.amount) + " G1's."
+      document.getElementById("gen1Cost").textContent = "It costs " + format(g1.cost) + " points to buy a G1."
     }
   }
 }
 function updateThings() {
-  document.getElementById("money").textContent = "You have " + money + " points."
-  document.getElementById("gen1Amount").textContent = "You have " + g1.amount + " G1's."
-  document.getElementById("gen1Cost").textContent = "It costs " + g1.cost + " points to buy a G1."
+  document.getElementById("money").textContent = "You have " + format(money) + " points."
+  document.getElementById("gen1Amount").textContent = "You have " + format(g1.amount) + " G1's."
+  document.getElementById("gen1Cost").textContent = "It costs " + format(g1.cost) + " points to buy a G1."
 }
 function format(num){
   let power = Math.floor(Math.log10(num))
@@ -33,6 +33,6 @@ function gameLoop(){
   updateThings()
 }
 setInterval(gameLoop,50)
-document.getElementById("money").textContent = "You have " + money + " points."
-document.getElementById("gen1Amount").textContent = "You have " + g1.amount + " G1's."
-document.getElementById("gen1Cost").textContent = "It costs " + g1.cost + " points to buy a G1."
+document.getElementById("money").textContent = "You have " + format(money) + " points."
+document.getElementById("gen1Amount").textContent = "You have " + format(g1.amount) + " G1's."
+document.getElementById("gen1Cost").textContent = "It costs " + format(g1.cost) + " points to buy a G1."
