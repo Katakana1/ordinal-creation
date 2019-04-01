cff0f644f490923be6dbc3038d662bf3b71b06f1
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-04-01 18:48:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -23,12 +23,26 @@ var g2 = {
     }
   }
 }
+var g3 = {
+  amount: 0,
+  cost: 100000,
+  buy: function() {
+    if(money < this.cost) return 0;
+    if(money >= this.cost) {
+      money-=this.cost
+      this.amount++
+      this.cost = Math.round(this.cost*1.1)
+    }
+  }
+}
 function updateThings() {
   document.getElementById("money").textContent = "You have " + format(money) + " points."
   document.getElementById("gen1Amount").textContent = "You have " + format(g1.amount) + " G1's."
   document.getElementById("gen1Cost").textContent = "It costs " + format(g1.cost) + " points to buy a G1."
   document.getElementById("gen2Amount").textContent = "You have " + format(g2.amount) + " G2's."
   document.getElementById("gen2Cost").textContent = "It costs " + format(g2.cost) + " points to buy a G2."
+  document.getElementById("gen3Amount").textContent = "You have " + format(g3.amount) + " G3's."
+  document.getElementById("gen3Cost").textContent = "It costs " + format(g3.cost) + " points to buy a G3."
 }
 function format(num){
   let power = Math.floor(Math.log10(num))
@@ -39,6 +53,7 @@ function format(num){
 function tick(diff){
   money+=(g1.amount*diff)
   g1.amount+=(g2.amount*diff)
+  g2.amount+=(g3.amount*diff)
 }
 function gameLoop(){
   tick(1/20)
